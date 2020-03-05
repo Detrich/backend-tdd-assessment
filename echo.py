5880050132a5023fbf0ad5353c956090bcb3187e
detrich <detrich2k@gmail.com>
2020-03-05 12:51:58 -0500

    completed TDD

--- echo.py
+++ echo.py
@@ -2,21 +2,55 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Detrich"
 
 
 import sys
+import argparse
+
+
+def toUppercase(text):
+    return text.upper()
+
+
+def toLowercase(text):
+    return text.lower()
+
+
+def toTitlecase(text):
+    return text.title()
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument('-u', '--upper', help='convert text to uppercase', action='store_true')
+    parser.add_argument('-l', '--lower', help='convert text to lowercase', action='store_true')
+    parser.add_argument('-t', '--title', help='convert text to titlecase', action='store_true')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+    text = ns.text
+    toUpper = ns.upper
+    toLower = ns.lower
+    toTitle = ns.title
+    if text is sys.argv[1]:
+        print(text)
+    if toTitle:
+        print(toTitlecase(text))
+    elif toLower:
+        print(toLowercase(text))
+    elif toUpper:
+        print(toUppercase(text))
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,10 +3,34 @@
 
 import unittest
 import echo
+import subprocess
 
-# Your test case class goes here
+
 class TestEcho(unittest.TestCase):
-    pass
+    def test_upper(self):
+        self.assertEqual(echo.toUppercase("hello!"), "HELLO!")
+        self.assertEqual(echo.toUppercase('"hola"'), '"HOLA"')
+
+    def test_lower(self):
+        self.assertEqual(echo.toLowercase("HELLO"), "hello")
+        self.assertEqual(echo.toLowercase('"HoLA"'), '"hola"')
+
+    def test_title(self):
+        self.assertEqual(echo.toTitlecase("hello"), "Hello")
+        self.assertEqual(echo.toTitlecase('"hola"'), '"Hola"')
+    
+    def test_text(self):
+        self.assertEqual(("hello"), "hello")
+
+    def test_help(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
 
 if __name__ == '__main__':
     unittest.main()
